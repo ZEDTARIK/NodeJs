05e3e10f01cd7de5a55c26547d480498b46d4926
zouhair <ettarak.zouhair@gmail.com>
2020-07-11 17:50:23 +0100

    add package Validation Joi and function validationEmployee

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express =require('express');
+const express = require('express');
+const Joi = require('joi');
 const { json } = require('express');
 const app = express();
-
 app.use(json());
 
 employees = [
-    {empId: 1 , employeeName: "Zouhair ETTARAK", salary: 9000},
-    {empId: 2, employeeName: "Hafssa ELMESKANI", salary: 2000 },
-    {empId: 3, employeeName: "Rachida ELHIDAOUI", salary: 6000 },
-]
-
+    { empId: 1, employeeName: "Zouhair ETTARAK", salary: 9000 },
+    { empId: 2, employeeName: "Hafssa ELMESKANI", salary: 2000 },
+    { empId: 3, employeeName: "Rachida ELHIDAOUI", salary: 6000 },
+];
 
 app.get('/', (req, res) => {
     res.send(employees);
@@ -17,25 +16,58 @@ app.get('/', (req, res) => {
 
 app.get('/employees/:id', (req, res) => {
     const findEmployee = employees.find(element => element.empId == req.params.id)
-    if (!findEmployee)
-    {
+    if (!findEmployee) {
         res.send("Employee Not Found");
     }
     res.send(findEmployee);
 });
 
+function employeeValidate(employee) {
+    const schema = {
+        empId: Joi.number().integer().required(),
+        employeeName: Joi.string().required(),
+        salary: Joi.number().integer().required()
+    };
 
-// Route : http://localhost:3000/employees
-app.post('/employees', (req, res)=> {
-    
+    return Joi.validate(employee, schema);
+}
+
+// POST  Route : http://localhost:3000/employees
+
+app.post('/employees', (req, res) => {
+    const { error } = employeeValidate(req.body);
+    if (error) {
+        return res.send(error.details[0].message)
+    }
     const employee = {
         empId: req.body.empId,
         employeeName: req.body.employeeName,
-        salary: req.body.salary        
+        salary: req.body.salary
     }
     employees.push(employee);
     res.send(employee)
 });
 
-const port =process.env.port || 3000
-app.listen(port, ()=> console.log('localhost:3000'));
+function employeePutValidate(employee) {
+    const schema = {
+        employeeName: Joi.string().required(),
+    };
+
+    return Joi.validate(employee, schema);
+}
+// PUT Route : http://localhost:3000/employees/id
+app.put('/employees/:id', (req, res) => {
+    const findEmployee = employees.find(element => element.empId == req.params.id)
+    if (!findEmployee) {
+       return  res.send("Employee Not Found");
+    }
+    const { error } = employeePutValidate(req.body);
+    if (error) {
+        return res.send(error.details[0].message)
+    }
+    findEmployee.employeeName = req.body.employeeName;
+    res.send(findEmployee);
+});
+
+const port = process.env.port || 3000
+app.listen(port, () => console.log('localhost:3000'));
